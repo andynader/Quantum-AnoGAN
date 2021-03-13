Fixed bugs in computation of x hat and in choosing rotation baseS

PyFiles/Generator.py
@@ -1,5 +1,7 @@
 import pennylane as qml
 from pennylane import numpy as np
+from pennylane.templates import StronglyEntanglingLayers
+from pennylane.init import strong_ent_layers_normal
 import numpy as scinp
 
 import torch
@@ -12,24 +14,32 @@ def state_preparation(latent_variables):
         qml.RX(latent_variables[i], wires=i)
 
 
-def layer(W):
+def generate_rotation_bases(num_layers, num_wires):
+    rotation_bases_all_layers = []
+    for i in range(num_layers):
+        rotation_bases_single_layer = [np.random.choice([qml.RX, qml.RY, qml.RZ]) for j in range(num_wires)]
+        rotation_bases_all_layers.append(rotation_bases_single_layer)
+    return rotation_bases_all_layers
+
+
+def layer(W, rot_bases):
     num_wires = len(W)
     for i in range(num_wires):
-        random_rot = np.random.choice([qml.RX, qml.RY, qml.RZ])
-        random_rot(W[i], wires=i)
+        rot = rot_bases[i]
+        rot(W[i], wires=i)
 
     for i in range(num_wires - 1):
         qml.CNOT(wires=[i, i + 1])
 
 
-def variational_circuit(latent_variables, weights):
+def variational_circuit(latent_variables, weights, rotation_bases_all_layers=None):
     # The number of wires is exactly equal to
     # the dimensionality of the latent variables.
 
     state_preparation(latent_variables)
 
-    for W in weights:
-        layer(W)
+    for i in range(len(weights)):
+        layer(weights[i], rotation_bases_all_layers[i])
 
     return [qml.expval(qml.PauliZ(i)) for i in range(len(latent_variables))]
 
@@ -48,10 +58,13 @@ class QuantumGenerator(nn.Module):
 
         self.device = device
         self.latent_dim = latent_dim
-        self.upscaling_dimension=upscaling_dimension
+
+        # We generate the rotation bases
+        self.rotation_bases_all_layers = generate_rotation_bases(num_layers=num_layers, num_wires=latent_dim)
+
+        self.upscaling_dimension = upscaling_dimension
         # We initialize and store the quantum classifier's weights
         W = torch.Tensor(num_layers, latent_dim).uniform_(-np.pi, np.pi).to(self.device)
-
         # We specify that the quantum classifier weights parameters of the
         # hybrid quantum classical generator, and thus should be differentiated.
 
@@ -65,6 +78,7 @@ class QuantumGenerator(nn.Module):
     def forward(self):
         # We define the latent variables, and pass them through a quantum generator.
         latent_variables = torch.Tensor(self.latent_dim).uniform_(-np.pi, np.pi).to(self.device)
-        quantum_out = self.vqc(latent_variables, self.quantum_weights).float().to(self.device)
+        quantum_out = self.vqc(latent_variables, self.quantum_weights, self.rotation_bases_all_layers).float().to(
+            self.device)
         generated_sample = self.upscaling_layer(quantum_out)
         return generated_sample

PyFiles/Training.py
@@ -1,5 +1,6 @@
 from Generator import *
 from Critic import *
+from Utility import *
 
 
 # All the sampled arrays returned are torch tensors.
@@ -25,7 +26,7 @@ def sample_arrays(X, generator, batch_size, upscaling_dimension, device):
 
 def get_X_hat(X_minibatch, quantum_gen_minibatch, epsilons):
     eps_new_axis = epsilons[:, np.newaxis]
-    X_hat = eps_new_axis * (X_minibatch - quantum_gen_minibatch)
+    X_hat = X_minibatch + eps_new_axis * (quantum_gen_minibatch - X_minibatch)
     return X_hat
 
 
@@ -57,18 +58,26 @@ def generator_loss(generator, critic, batch_size, device):
     for k in range(batch_size):
         sample = generator().unsqueeze(0)
         quantum_gen_minibatch = torch.cat((quantum_gen_minibatch, sample))
-    L_g = -critic(quantum_gen_minibatch)
+    L_g = -critic(quantum_gen_minibatch).flatten()
     return torch.sum(L_g) / batch_size
 
 
+def monitor_generated_samples(generator, n_monitoring_samples=10):
+    sum_of_samples = scinp.zeros(generator.upscaling_dimension)
+    for i in range(n_monitoring_samples):
+        sum_of_samples += generator().cpu().detach().numpy()
+    average_sample = sum_of_samples / n_monitoring_samples
+    print("Generated Average Sample:", average_sample)
+
+
 def train_quantum_anogan(X, generator: QuantumGenerator, critic: Critic, device, n_iter=1, batch_size=64,
-                         n_critic=5):
+                         n_critic=5, n_monitoring_samples=3):
     num_features = X.shape[1]
     upscaling_dimension = generator.upscaling_dimension
 
     assert upscaling_dimension == num_features
-    generator_optimizer = torch.optim.Adam(generator.parameters())
-    critic_optimizer = torch.optim.Adam(critic.parameters())
+    generator_optimizer = torch.optim.Adam(generator.parameters(), lr=0.001, betas=(0.5, 0.999), eps=1e-7)
+    critic_optimizer = torch.optim.Adam(critic.parameters(), lr=0.001, betas=(0.5, 0.999), eps=1e-7)
     for i in range(n_iter):
         print("Iteration ", i + 1)
         for j in range(n_critic):
@@ -88,4 +97,6 @@ def train_quantum_anogan(X, generator: QuantumGenerator, critic: Critic, device,
         generator_optimizer.zero_grad()
         gen_loss.backward()
         generator_optimizer.step()
+        # print_parameters(generator)
+        monitor_generated_samples(generator, n_monitoring_samples)
     return generator, critic

PyFiles/main.py
@@ -2,26 +2,21 @@ from Generator import *
 from Critic import *
 from Training import *
 
-X_normal = scinp.random.uniform(low=-10, high=-5, size=(100, 4))
+X_normal = scinp.random.uniform(low=1, high=2, size=(1000, 4))
 X_anomalous = scinp.random.uniform(low=50, high=100, size=(100, 4))
 
 latent_dim = 2
 num_layers = 2
 upscaling_dimension = 4
-batch_size = 5
+batch_size = 64
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Using {} device".format(device))
 
 quantum_gen = QuantumGenerator(latent_dim, num_layers, upscaling_dimension, device)
 quantum_gen = quantum_gen.to(device)
 
-
 critic = Critic(data_dimension=upscaling_dimension)
 critic = critic.to(device)
 
-quantum_gen, critic = train_quantum_anogan(X_normal, quantum_gen, critic, device, n_iter=10, batch_size=64, n_critic=5)
-
-
-generated_samples = [quantum_gen.forward().cpu().detach().numpy() for i in range(10)]
-for i in range(len(generated_samples)):
-    print("Sample {}:\t{}\n".format(i + 1, generated_samples[i]))
+quantum_gen, critic = train_quantum_anogan(X_normal, quantum_gen, critic, device,
+                                           n_iter=100, batch_size=batch_size, n_critic=10, n_monitoring_samples=5)